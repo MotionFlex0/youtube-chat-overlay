09b52edc38923821ead82ef8899caeacc4028b01
MotionFlex0 <31831208+MotionFlex0@users.noreply.github.com>
2021-02-01 22:02:28 +0000

    Overlay captures messages and display them on video

--- src/js/content_scripts/youtube_overlay.js
+++ src/js/content_scripts/youtube_overlay.js
@@ -1,63 +1,116 @@
-console.log(`This is the content script overlay..>! URL: ${window.location.href}`);
+const CHAT_ITEM_OBSERVER_OPTIONS = {
+    root: document.querySelector(".yco-chat-item-list"),
+    threshold: 0.1
+};
 
-const chatFrame = document.querySelector("#chatframe");
+const chatItemIntersectionObserver = new IntersectionObserver(chatItemIntersectionCallback, CHAT_ITEM_OBSERVER_OPTIONS);
 
-if (chatFrame) {
-    const chatApp = chatFrame.contentDocument.querySelector("yt-live-chat-app");
-    if (chatApp) {
-        console.log("[youtube-chat-overlay] Found chat app.");
-        const chatHeader = chatApp.querySelector("yt-live-chat-header-renderer");
-        const chat = chatApp.querySelector("#chat");
 
-        const chatItem = chat.querySelector("#items");
+(async () => {
+    await new Promise(resolve => setTimeout(resolve, 5000));
 
-        const observer = new MutationObserver(chatObserverCallback);
-        observer.observe(chatItem, {
-            childList: true
-        });
-
-        const youtubePlayer = document.querySelector("#ytd-player");
-        if (youtubePlayer.clientWidth > 0 && youtubePlayer.clientHeight > 0) {
-            fetch(chrome.runtime.getURL("/src/template/css/style.css"))
-            .then(resp => resp.text())
-            .then(css => {
-                document.head.insertAdjacentHTML("beforeend", `<style>${css}</style>`);
-            });  
-
-            // This is purely for testing. It prevents the chat overlay being loaded more than once.
-            const chatOverlay = document.querySelector(".yco-chat");
-            if (chatOverlay == null) {
-                fetch(chrome.runtime.getURL("/src/template/chat_overlay.html"))
+    console.log(`This is the content script overlay..>! URL: ${window.location.href}`);
+    
+    const chatFrame = document.querySelector("#chatframe");
+    
+    if (chatFrame) {
+        const chatApp = chatFrame.contentDocument.querySelector("yt-live-chat-app");
+        if (chatApp) {
+            console.log("[youtube-chat-overlay] Found chat app.");
+            const chatHeader = chatApp.querySelector("yt-live-chat-header-renderer");
+            const chat = chatApp.querySelector("#chat");
+    
+            const chatItem = chat.querySelector("#items");
+    
+            const observer = new MutationObserver(chatObserverCallback);
+            observer.observe(chatItem, {
+                childList: true
+            });
+            
+            const youtubePlayer = document.querySelector("#ytd-player");
+            if (youtubePlayer.clientWidth > 0 && youtubePlayer.clientHeight > 0) {
+                fetch(chrome.runtime.getURL("/src/template/css/style.css"))
                 .then(resp => resp.text())
-                .then(template => {
-                    youtubePlayer.insertAdjacentHTML("beforeend", template);
-                });
+                .then(css => {
+                    document.head.insertAdjacentHTML("beforeend", `<style>${css}</style>`);
+                });  
+    
+                // This is purely for testing. It prevents the chat overlay being loaded more than once.
+                const chatOverlay = document.querySelector(".yco-chat");
+                if (chatOverlay == null) {
+                    fetch(chrome.runtime.getURL("/src/template/chat_overlay.html"))
+                    .then(resp => resp.text())
+                    .then(template => {
+                        youtubePlayer.insertAdjacentHTML("beforeend", template);
+                    });
+                }
+    
             }
-
+        }
+        else {
+            console.log("[youtube-chat-overlay] Something went wrong! Found chat frame but could not find chat app.");
         }
     }
-    else {
-        console.log("[youtube-chat-overlay] Something went wrong! Found chat frame but could not find chat app.");
-    }
-}
+})();
 
 
 function chatObserverCallback(mutationList, observer) {
     mutationList.forEach(mutation => {
         if (mutation.addedNodes.length > 0) {
             console.log(`[youtube-chat-overlay] Node(s) added with length: ${mutation.addedNodes.length}`);
+            setTimeout(() => {
+                mutation.addedNodes.forEach(node => {
+                    chatQueue.addMessageFromRendererElement(node)
+                });
+            }, 500);
         }
         if (mutation.removedNodes.length > 0) {
             console.log(`[youtube-chat-overlay] Node(s) removed with length: ${mutation.removedNodes.length}`)
+            setTimeout(() => {
+                mutation.removedNodes.forEach(node => chatQueue.removeMessageFromRendererElement(node));
+            }, 500);
         }
     });
 }
 
-const chatOverlayQueue = {
-    pushItem: function(e) {
-        
-    },
-    popItem: function() {
+function chatItemIntersectionCallback(entries, observer) {
+    entries.forEach(e => {
+        if (e.intersectionRatio != 1) {
+            // console.log("something has gone out of view. element: ", e.target.querySelector(".message").innerText)
+            e.target.remove();
+        }
+    });
+}
 
+const chatQueue = {
+    addMessageFromRendererElement: function(rendererElement) {
+        const ycoChatList = document.querySelector(".yco-chat-item-list");
+        const newChatItem = ycoChatList.querySelector(".yco-chat-item[hidden]").cloneNode(true);
+        // for (const [k,v] of chatItems) {
+        //     if ()
+        // }
+        const authorPhoto = rendererElement.querySelector("#author-photo > img").src;
+        const timestamp = rendererElement.querySelector("#timestamp").innerText;
+        const authorName = rendererElement.querySelector("#author-name").innerText;
+        const message = rendererElement.querySelector("#message").innerHTML;
+
+        
+        // console.log({authorPhoto, timestamp, authorName, message});
+        //console.log(rendererElement);
+
+        newChatItem.querySelector(".author-name").innerText = authorName;
+        newChatItem.querySelector(".message").innerHTML = message;
+        newChatItem.removeAttribute("hidden");
+        ycoChatList.appendChild(newChatItem);
+        ycoChatList.scrollTop = ycoChatList.scrollHeight;
+        chatItemIntersectionObserver.observe(newChatItem);
+        // authorName.innerText = 
+        //authorName.children[0].replaceWith(document.adoptNode(rendererElement));
+        //message.innerText = rendererElement.querySelector("#message").innerText;
+
+    },
+    removeMessageFromRendererElement: function(rendererElement) {
+        const ycoChatList = document.querySelector(".yco-chat-item-list");
+        ycoChatList.querySelector(`#${rendererElement.id}`).remove(); 
     }
 }
