fd38c4aa54cc46e6f8f4362d8c2349a08511997c
MotionFlex0 <31831208+MotionFlex0@users.noreply.github.com>
2021-04-28 23:55:49 +0100

    Added a basic WinBox for testing

--- src/js/content_scripts/yco_settings.js
+++ src/js/content_scripts/yco_settings.js
@@ -1,4 +1,30 @@
 //Inject settings menu into YouTube video
-(() => {
-    
-})();
+
+console.log("Inside yco_settings.js....");
+
+setTimeout(() => {
+    fetch(chrome.runtime.getURL("/src/js/lib/winbox.bundle.js"))
+    .then(resp => resp.text())
+    .then(js => {
+        // document.body.insertAdjacentHTML("beforeend", `<script>"${js}"></script></script>`);
+        window.eval(js);
+        console.log("Injected WinBox into page....");
+        
+        const settingsWindow = new WinBox("YCO Settings", {
+            class: ["no-full"],
+            x: "center",
+            y: "center",
+            top: 40,
+            max: false,
+            onmoove: function(x, y) {
+                this.body.textContent = 
+                    "x: " + x + ", " +
+                    "y: " + y
+                ;
+            }
+        });
+        // settingsWindow.move(, "center");
+    });
+
+
+}, 3000);
